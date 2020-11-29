756fd8456ffc32583ee318b25066e20a2d15c491
akki907 <ak.akki907@gmail.com>
2020-11-29 09:58:39 +0530

    intial commit

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const express = require('express')
 
 // load the env 
 const dotEnv = require('dotenv')
+const router = require('./routes')
 dotEnv.config()
 // end
 
-const cors = require('cors')
+// const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT || 5000
+
+
+app.use('/api/v1',router)
+
+app.get('/',(req,res)=>{
+    res.status(400).send({tets:'dasda'})
+})
 
 
 app.listen(PORT,console.log(`Server is running on ${process.env.ENV} mode at ${PORT}`))
